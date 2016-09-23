587d61344a778b49fbeab35c928f97d5fef9cd24
KienThuc <thuc.ttk@gmail.com>
2016-09-23 13:39:15 +0700

    Scenes

---
+++ SourceCode/New Unity Project/Assets/Script/Menu/Option.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Option : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public void SwitchSceneOption()
+	{
+		Application.LoadLevel ("Option");
+	}
+	public void SwitchSceneMenu()
+	{
+		Application.LoadLevel ("Menu");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
